1b29426f16ea59e56d3529ac75696316bddec706
Zachary R <vaccadeus@gmail.com>
2020-05-23 17:06:48 +1000

    Teleportation - lostgem now teleports player to the new dimension on use in the water

--- main/java/vespi/lostcity/items/ElderSoul.java
+++ main/java/vespi/lostcity/items/ElderSoul.java
@@ -1,16 +1,6 @@
 package vespi.lostcity.items;
 
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.world.World;
-import net.minecraft.world.dimension.DimensionType;
-import vespi.lostcity.dimension.ModDimensions;
-import vespi.lostcity.dimension.TutorialDimension;
 import vespi.lostcity.setup.ModSetup;
 
 public class ElderSoul extends Item {
@@ -19,20 +9,5 @@ public class ElderSoul extends Item {
                 .maxStackSize(16)
                 .group(ModSetup.itemGroup));
         setRegistryName("eldersoul");
-    }
-
-    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
-        if (!worldIn.isRemote) {
-            if (playerIn.isInWater()) {
-                if (worldIn.dimension.getType() == ModDimensions.DIMENSION_TYPE) {
-                    playerIn.changeDimension(DimensionType.OVERWORLD);
-                } else {
-                    playerIn.changeDimension(ModDimensions.DIMENSION_TYPE);
-                }
-            } else {
-                playerIn.sendMessage(new StringTextComponent("The Soul seems to pull you toward the water... but not much else happens"));
-            }
-        }
-        return new ActionResult<>(ActionResultType.SUCCESS, playerIn.getHeldItem(handIn));
     }
 }
--- main/java/vespi/lostcity/items/LostGem.java
+++ main/java/vespi/lostcity/items/LostGem.java
@@ -1,7 +1,14 @@
 package vespi.lostcity.items;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
-import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionType;
+import vespi.lostcity.dimension.ModDimensions;
 import vespi.lostcity.setup.ModSetup;
 
 public class LostGem extends Item {
@@ -14,6 +21,21 @@ public class LostGem extends Item {
 
         setRegistryName("lostgem");
 
+        }
+
+        public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+            if (!worldIn.isRemote) {
+                if (playerIn.isInWater()) {
+                    if (worldIn.dimension.getType() == ModDimensions.DIMENSION_TYPE) {
+                        playerIn.changeDimension(DimensionType.OVERWORLD);
+                    } else {
+                        playerIn.changeDimension(ModDimensions.DIMENSION_TYPE);
+                    }
+                } else {
+                    playerIn.sendMessage(new StringTextComponent("The Soul seems to pull you toward the water... but not much else happens"));
+                }
+            }
+            return new ActionResult<>(ActionResultType.SUCCESS, playerIn.getHeldItem(handIn));
         }
     }
 
